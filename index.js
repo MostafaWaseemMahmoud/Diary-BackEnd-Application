d350619e69ba320e012fe5108f045e0cdf8f422f
MostafaWaseemMahmoud <mostafawaseem22@gmail.com>
2024-04-30 02:11:17 +0300

    commit Application Now

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 app.use(express.json());
 const users = [];
+
+const corsOptions = {
+  origin: "http://localhost:4200", // Allow requests only from this origin
+  optionsSuccessStatus: 200, // Some legacy browsers choke on 204
+};
+
+app.use(cors(corsOptions));
 
 const port = 8080 || process.env.PORT;
 
